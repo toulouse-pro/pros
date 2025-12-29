fb77da69291f0c86fafc16673633c717a5d537ab
Toulouse-Pro <31000web@gmail.com>
2025-12-29 14:42:27 +0100

    Create schema.js

---
+++ editor/schema.js
@@ -0,0 +1,9 @@
+const SCHEMA = {
+  accueil: {
+    file: "_data/accueil.yml", // Make sure this path matches your GitHub exactly
+    fields: [
+      { name: "titre", label: "Titre", type: "text" },
+      { name: "description", label: "Description", type: "textarea" }
+    ]
+  }
+};
